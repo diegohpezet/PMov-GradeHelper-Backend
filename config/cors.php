24897fc781b05b3607d5693268b3ebd8f5ab9cc8
diegohpezet <diegohpezet@gmail.com>
2024-10-10 17:08:50 -0300

    add cors config

---
+++ config/cors.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['http://127.0.0.1:5173'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
+];
