cc4022d9bec85ab110f33338681d20dba7caf2a2
Tehuel <torresbaldi.tehuel@gmail.com>
2024-10-10 16:29:45 -0300

    update seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Course;
+use App\Models\Exercise;
+use App\Models\Student;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(ExerciseSeeder::class);
-        $this->call(CourseSeeder::class);
+        $courses = $courses = Course::factory()
+            ->count(3)
+            ->hasStudents(10)
+            ->create();
+
+        Exercise::factory(5)->hasAttached($courses)->create();
     }
 }
