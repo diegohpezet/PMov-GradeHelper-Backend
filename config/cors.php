cc2b3d6c1ecfd46b53d711cd33fec6b5d2541270
diegohpezet <diegohpezet@gmail.com>
2024-10-12 12:52:23 -0300

    chore: replace static allowed origin for env variable

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://127.0.0.1:5173'],
+    'allowed_origins' => [env('APP_FRONTEND_URL')],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
