60cf3a739cf98f7a89f2f4c91931404aba0ef993
Tehuel <torresbaldi.tehuel@gmail.com>
2024-10-13 12:06:45 -0300

    uses same return format on all methods

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -12,7 +12,9 @@ class CourseController extends Controller
      */
     public function index()
     {
-        return Course::all();
+        $courses = Course::all();
+
+        return response()->json($courses);
     }
 
     /**
