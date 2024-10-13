dbdeb8b6bfa3cfa76fbe4a60654fafd26c3d4ce0
Tehuel <torresbaldi.tehuel@gmail.com>
2024-10-13 11:56:04 -0300

    use alternative to "morphs" for uuid

--- database/migrations/2024_10_02_175547_create_personal_access_tokens_table.php
+++ database/migrations/2024_10_02_175547_create_personal_access_tokens_table.php
@@ -13,8 +13,7 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->string('tokenable_id', 36);
-            $table->string('tokenable_type');
+            $table->uuidMorphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
