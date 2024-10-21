d56bc17b9e8991eb2b1db93281cd7b04251e4a9e
diegohpezet <diegohpezet@gmail.com>
2024-10-21 12:59:15 -0300

    update migration timestamp and add new fields to students table

--- database/migrations/2024_10_05_162030_create_students_table.php
+++ database/migrations/2024_10_05_162030_create_students_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->string('first_name');
             $table->string('last_name');
-
+            $table->string('githubUsername')->nullable()->unique();
+            
+            $table->foreignUuid('user_id')->nullable()->constrained();
             $table->foreignUuid('course_id')->constrained();
             $table->timestamps();
         });
