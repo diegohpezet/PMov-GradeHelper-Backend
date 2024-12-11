a636a110e1093cdf10ed721da56d7f6d70c50c50
diegohpezet <diegohpezet@gmail.com>
2024-12-11 12:35:30 -0300

    courses inertia implementation

---
+++ resources/js/Pages/Courses/Edit.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useForm } from '@inertiajs/vue3';
+
+const props = defineProps({ course: Object })
+
+const form = useForm(props.course)
+
+const editCourse = (id) => {
+  form.put(`/courses/${id}`)
+}
+</script>
+
+<template>
+  <form @submit.prevent="editCourse(course.id)">
+    <div class="mb-3">
+      <label for="name" class="form-label">Name</label>
+      <input type="text" class="form-control" id="name" v-model="form.name">
+    </div>
+
+    <div class="mb-3">
+      <label for="school_year" class="form-label">School year</label>
+      <input type="text" class="form-control" id="school_year" v-model="form.school_year">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+    
+  </form>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
+
+use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
+Route::resource('courses', CourseController::class);
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
