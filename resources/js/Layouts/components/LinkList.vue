17c701b96cae45a177a6904d3ca56fbb2794d0df
Tehuel <torresbaldi.tehuel@gmail.com>
2024-12-14 11:45:44 -0300

    remove span and ul

--- resources/js/Layouts/components/LinkList.vue
+++ resources/js/Layouts/components/LinkList.vue
@@ -3,27 +3,31 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-  <ul class="list-group list-group-flush bg-transparent">
-      <Link class="list-group-item" href="/courses">
-        <span><i class="ri-file-list-3-line me-2"></i>Courses</span>
-      </Link>
-      <Link class="list-group-item" href="/exercises">
-        <span><i class="ri-question-answer-line me-2"></i>Exercises</span>
-      </Link>
-      <Link class="list-group-item" href="/students">
-        <span><i class="ri-graduation-cap-line me-2"></i>Students</span>
-      </Link>
-      <Link class="list-group-item" href="/grades">
-        <span><i class="ri-numbers-line me-2"></i>Grades</span>
-      </Link>
-      <hr />
-      <button class="list-group-item text-start">
-        <span><i class="ri-sun-line me-2"></i>Switch theme</span>
-      </button>
-      <button class="list-group-item text-start">
-        <span><i class="ri-logout-circle-line me-2"></i>Log out</span>
-      </button>
-    </ul>
+  <div class="list-group list-group-flush">
+    <Link class="list-group-item" href="/courses">
+      <i class="ri-file-list-3-line me-2"></i>
+      Courses
+    </Link>
+    <Link class="list-group-item" href="/exercises">
+      <i class="ri-question-answer-line me-2"></i>
+      Exercises
+    </Link>
+    <Link class="list-group-item" href="/students">
+      <i class="ri-graduation-cap-line me-2"></i>
+      Students
+    </Link>
+    <Link class="list-group-item" href="/grades">
+      <i class="ri-numbers-line me-2"></i>
+      Grades
+    </Link>
+    <hr/>
+    <button class="list-group-item text-start">
+      <i class="ri-sun-line me-2"></i>Switch theme
+    </button>
+    <button class="list-group-item text-start">
+      <i class="ri-logout-circle-line me-2"></i>Log out
+    </button>
+  </div>
 </template>
 
 <style scoped>
