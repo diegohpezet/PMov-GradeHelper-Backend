8386252504ee710ad0bda17105051e4f5191fdce
diegohpezet <diegohpezet@gmail.com>
2024-12-17 11:32:44 -0300

    default route redirects to courses when logged in

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,14 @@ use App\Http\Controllers\CourseController;
 use App\Http\Controllers\ExerciseController;
 use App\Http\Controllers\GradeController;
 use App\Http\Controllers\StudentController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
+Route::get('/', function (Request $request) {
+    if ($request->user()) {
+        return redirect()->route('courses.index');
+    }
     return Inertia::render('Welcome');
 });
 
