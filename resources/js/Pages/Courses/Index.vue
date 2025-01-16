2c4fdf8ab6f8ead23840e9e1cdad04855cd310c0
Tehuel <torresbaldi.tehuel@gmail.com>
2025-01-16 00:42:37 -0300

    adds course item card vue component

--- resources/js/Pages/Courses/Index.vue
+++ resources/js/Pages/Courses/Index.vue
@@ -1,18 +1,14 @@
 <script setup>
-import { Link, router, usePage } from '@inertiajs/vue3';
+import { Link, usePage } from '@inertiajs/vue3';
 import { computed } from 'vue';
 import CreateCourseForm from './components/CreateCourseForm.vue';
+import CourseItemCard from './components/CourseItemCard.vue';
 
 const props = defineProps({ courses: [Object] });
 
 const page = usePage();
 const isAdmin = computed(() => page.props.auth.isAdmin);
 
-const deleteCourse = (id) => {
-  if (confirm('Are you sure you want to delete this course?')) {
-    router.delete(`/courses/${id}`);
-  }
-};
 </script>
 
 <template>
@@ -25,40 +21,11 @@ const deleteCourse = (id) => {
     <CreateCourseForm />
   </details>
 
-  <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 mt-3 g-3">
-    <div class="col" v-for="course in courses" :key="course.id">
-      <div class="card h-100">
-        <div class="card-body row">
-
-          <div class="col">
-            <Link :href="`/courses/${course.id}`" class="card-title fs-4">{{ course.name }}</Link>
-            <p class="card-text text-muted">{{ course.school_year }}</p>
-          </div>
-
-          <div v-if="isAdmin" class="col-auto btn-group text-end">
-            <button type="button" class="btn dropdown-toggle rounded-circle border my-auto" 
-              data-bs-toggle="dropdown" aria-expanded="false">
-            </button>
-
-            <ul class="dropdown-menu">
-              <li>
-                <Link class="dropdown-item" :href="`/courses/${course.id}/edit`">
-                  <i class="ri ri-pencil-line"></i>
-                  <span>Edit course</span>
-                </Link>
-              </li>
-              <li>
-                <button class="dropdown-item" @click="deleteCourse(course.id)">
-                  <i class="ri ri-delete-bin-line"></i>
-                  <span>Delete course</span>
-                </button>
-              </li>
-            </ul>
-
-          </div>
-
-        </div>
-      </div>
-    </div>
+  <div class="">
+    <CourseItemCard 
+      v-for="course in courses" 
+      :key="course.id"
+      :course="course"
+    />
   </div>
 </template>
---
+++ resources/js/Pages/Courses/components/CourseItemCard.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { Link, router, usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
+
+const page = usePage();
+const isAdmin = computed(() => page.props.auth.isAdmin);
+
+const props = defineProps({ 
+  course: [Object] 
+});
+
+const deleteCourse = (id) => {
+  if (confirm('Are you sure you want to delete this course?')) {
+    router.delete(`/courses/${id}`);
+  }
+};
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div class="row justify-content-between align-items-center">
+        <div class="col" style="position: relative;">
+          <Link :href="`/courses/${course.id}`" class="card-title fs-4 stretched-link">{{ course.name }}</Link>
+          <p class="card-text text-muted">{{ course.school_year }}</p>        
+        </div>
+        <div class="col-auto">
+          <div class="btn-group">
+            <Link
+              class="btn btn-outline-secondary" 
+              :href="`/courses/${course.id}/edit`"
+            >
+              <i class="ri ri-pencil-line"></i>
+              <span class="visually-hidden">Edit course</span>
+            </Link>
+            <button
+              class="btn btn-outline-danger" 
+              @click="deleteCourse(course.id)"
+            >
+              <i class="ri ri-delete-bin-line"></i>
+              <span class="visually-hidden">Delete course</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
