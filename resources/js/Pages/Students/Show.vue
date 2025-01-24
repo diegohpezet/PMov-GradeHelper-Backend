d092b58ef65cc5a4ee15328ee1385ff4b7d8b988
Tehuel <torresbaldi.tehuel@gmail.com>
2025-01-24 00:11:27 +0000

    refactor show student

--- resources/js/Pages/Students/Show.vue
+++ resources/js/Pages/Students/Show.vue
@@ -1,20 +1,23 @@
 <script setup>
-import { Link } from '@inertiajs/vue3';
-import GradesStudentsMatrix from '../Grades/Index.vue';
+import ExerciseRow from './components/ExerciseRow.vue';
 
-const props = defineProps({ student: Object, exercises: [Object] });
+const props = defineProps({ 
+  student: Object, 
+});
 </script>
 
 <template>
   <header class="mb-3 text-center">
-    <h1 class="h2">{{ student.last_name + ' ' + student.first_name }}</h1>
-    <sub class="text-muted fs-5">
-      <p class="mb-4">@{{ student.githubUsername }}</p>
-      <!-- <p>Course: <Link :href="`/courses/${student.course.id}`">{{ student.course.name }}</Link></p> -->
-    </sub>
+    <h1 class="fs-4 m-0">{{ student.last_name + ' ' + student.first_name }}</h1>
+    <p class="fs-5 text-muted">
+      @{{ student.githubUsername }}
+    </p>
   </header>
-
   <main>
-    <GradesStudentsMatrix :students="[student]" :exercises="exercises" />
+    <ExerciseRow
+      v-for="exercise in student.course.exercises" 
+      :key="exercise.id"
+      :exercise="exercise"
+    />
   </main>
 </template>
---
+++ resources/js/Pages/Students/components/ExerciseRow.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  exercise: Object,
+});
+</script>
+
+<template>
+  <div>
+    <div class="d-flex mb-2">
+      <h2 class="h3">{{ exercise.title }}</h2>
+    </div>
+
+    <ul class="list-group" v-if="exercise.grades.length">
+      <li class="list-group-item" v-for="gr in exercise.grades">
+        <div class="row align-items-center">
+          <div class="col-1" :class="{
+            'text-danger': gr.result == 'failed',
+            'text-success': gr.result == 'passed',
+          }">
+            {{ gr.result }}
+          </div>
+          <div class="col">{{ gr.comment }}</div>
+          <div class="col-2">{{ (new Date(gr.created_at)).toLocaleString() }}</div>
+        </div>
+      </li>
+    </ul>
+    <p v-else>No grades for this exercise.</p>
+  </div>
+</template>
