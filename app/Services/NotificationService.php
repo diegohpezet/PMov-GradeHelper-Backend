28f55565af347a28092c67dcd6b95384050d10db
diegohpezet <diegohpezet@gmail.com>
2025-01-28 12:59:12 -0300

    trait and service for notification

---
+++ app/Services/NotificationService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use App\Notifications\AdminActionNotification;
+use Illuminate\Support\Facades\Auth;
+
+class NotificationService
+{
+    public function notifyAdmins(array $actionDetails)
+    {
+        $admins = User::role('admin')->where('id', '!=', Auth::user()->id)->get();
+
+        foreach ($admins as $admin) {
+            $admin->notify(new AdminActionNotification($actionDetails));
+        }
+    }
+
+    // Maybe add notification for students in the future?
+}
