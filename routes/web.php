43dd866a395bf4caaf7c72da789ed5c4675937dd
diegohpezet <diegohpezet@gmail.com>
2025-02-07 22:36:34 -0300

    fix index route missing on admin group function

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,9 @@ Route::middleware('auth')->group(function () {
 
     Route::middleware('admin')->group(function () {
         Route::resource('courses', CourseController::class)->only(['store', 'edit', 'update', 'destroy']);
-        Route::resource('exercises', ExerciseController::class)->only(['store', 'edit', 'update', 'destroy']);
-        Route::resource('students', StudentController::class)->only(['store', 'edit', 'update', 'destroy']);
-        Route::resource('grades', GradeController::class)->only(['store', 'edit', 'update', 'destroy']);
+        Route::resource('exercises', ExerciseController::class)->only(['index','store', 'edit', 'update', 'destroy']);
+        Route::resource('students', StudentController::class)->only(['index','store', 'edit', 'update', 'destroy']);
+        Route::resource('grades', GradeController::class)->only(['index','store', 'edit', 'update', 'destroy']);
 
         Route::post('attendances', [AttendanceController::class, 'store'])->name('attendances.store');
         Route::delete('attendances', [AttendanceController::class, 'destroy'])->name('attendances.destroy');
