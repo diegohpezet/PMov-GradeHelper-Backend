c2fb47f2a1e31b45ae9a469f597585a886b14ce1
Tehuel <torresbaldi.tehuel@gmail.com>
2025-02-09 09:54:15 -0300

    adds factory

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Attendance extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['student_id', 'course_id', 'date'];
 
     public function student()
---
+++ database/factories/AttendanceFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Course;
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Attendance>
+ */
+class AttendanceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'student_id' => Student::factory(),
+            'course_id' => Course::factory(),
+            'date' => now()
+                ->add(fake()->numberBetween(-100, 100), 'day')
+                ->toDateString(),
+        ];
+    }
+}
