d6c50422f55d7590a161074bee56987b2a5cea8e
diegohpezet <diegohpezet@gmail.com>
2025-02-10 12:14:35 -0300

    PassFail grades factory

--- app/Models/PassFailGrade.php
+++ app/Models/PassFailGrade.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PassFailGrade extends Model
 {
+    use HasFactory;
+
+    protected $table = 'pass_fail_grades';
+
     protected $fillable = ['value', 'comment'];
 
     public function gradeable()
