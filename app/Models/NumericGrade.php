98cee051a3c9bcdc04d824182fcb042063f493e5
diegohpezet <diegohpezet@gmail.com>
2025-02-10 12:14:17 -0300

    Numeric grades factory

--- app/Models/NumericGrade.php
+++ app/Models/NumericGrade.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class NumericGrade extends Model
 {
+    use HasFactory;
+
+    protected $table = 'numeric_grades';
+
     protected $fillable = ['value', 'comment'];
 
     public function gradeable()
