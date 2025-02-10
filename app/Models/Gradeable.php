1f0827c50eaa0403d522a1432298faf2b11149ee
diegohpezet <diegohpezet@gmail.com>
2025-02-10 12:15:34 -0300

    gradeable HasUuid trait

--- app/Models/Gradeable.php
+++ app/Models/Gradeable.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Gradeable extends Model
 {
+    use HasUuids;
+    
     protected $fillable = ['assessment_id', 'student_id', 'gradable_id', 'gradable_type'];
 
     public function gradeable()
