450cdf0be49fabc3a57afd9a8ebbb49920fda97d
Tehuel <torresbaldi.tehuel@gmail.com>
2025-02-16 15:20:04 -0300

    remove comments

--- lang/es/passwords.php
+++ lang/es/passwords.php
@@ -1,18 +1,6 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | outcome such as failure due to an invalid password / reset token.
-    |
-    */
-
     'reset' => 'Tu contraseña ha sido restablecida.',
     'sent' => 'Hemos enviado por correo electrónico el enlace para restablecer tu contraseña.',
     'throttled' => 'Por favor, espera antes de volver a intentarlo.',
