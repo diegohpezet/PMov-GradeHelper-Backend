3c612956f20abc3d29ee7aae0f2af0826e6f32d6
Tehuel <torresbaldi.tehuel@gmail.com>
2025-02-16 21:45:39 -0300

    student grades

--- resources/js/Pages/Students/components/ExerciseRow.vue
+++ resources/js/Pages/Students/components/ExerciseRow.vue
@@ -15,18 +15,33 @@ const showGradeForm = ref(false);
   <div class="mb-3">
     <div class="d-flex align-items-center mb-2">
       <h2 class="h3">{{ exercise.title }}</h2>
-      <button class="btn btn-sm btn-outline-secondary ms-1" v-if="!showGradeForm" @click="showGradeForm = true">
-        Add Grade
+      <button
+        class="btn btn-sm btn-outline-secondary ms-1"
+        v-if="!showGradeForm"
+        @click="showGradeForm = true"
+      >
+        {{ $t('grades.add') }}
       </button>
     </div>
 
-    <AddGradeForm class="mb-3" v-if="showGradeForm" :studentId="studentId" :exerciseId="exercise.id" @close="showGradeForm = false" />
+    <AddGradeForm
+      class="mb-3"
+      v-if="showGradeForm"
+      :studentId="studentId"
+      :exerciseId="exercise.id"
+      @close="showGradeForm = false"
+    />
 
     <ul class="list-group" v-if="exercise.grades.length">
       <li class="list-group-item" v-for="gr in exercise.grades">
         <GradeRow :grade="gr" />
       </li>
     </ul>
-    <p v-else>No grades for this exercise.</p>
+    <p
+      class="text-muted"
+      v-else
+    >
+      {{ $t('grades.empty') }}
+    </p>
   </div>
 </template>
