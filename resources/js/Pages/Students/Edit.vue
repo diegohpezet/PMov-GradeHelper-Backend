59c328d3794715cf9eafa618606712cf8ecff9c0
Tehuel <torresbaldi.tehuel@gmail.com>
2025-02-16 21:25:31 -0300

    edit student

--- resources/js/Pages/Students/Edit.vue
+++ resources/js/Pages/Students/Edit.vue
@@ -16,32 +16,44 @@ const editStudent = (id) => {
 <template>
   <form @submit.prevent="editStudent(student.id)">
     <div class="mb-3">
-      <label for="first_name" class="form-label">First Name</label>
+      <label for="first_name" class="form-label">
+        {{ $t('students.field.first_name') }}
+      </label>
       <input type="text" class="form-control" id="first_name" v-model="form.first_name">
       <span v-if="form.errors.first_name" class="text-danger">{{ form.errors.first_name }}</span>
     </div>
 
     <div class="mb-3">
-      <label for="last_name" class="form-label">Last Name</label>
+      <label for="last_name" class="form-label">
+        {{ $t('students.field.last_name') }}
+      </label>
       <input type="text" class="form-control" id="last_name" v-model="form.last_name">
       <span v-if="form.errors.last_name" class="text-danger">{{ form.errors.last_name }}</span>
     </div>
 
     <div class="mb-3">
-      <label for="github_username" class="form-label">Github</label>
+      <label for="github_username" class="form-label">
+        {{ $t('students.field.github_username') }}
+      </label>
       <input type="text" class="form-control" id="github_username" v-model="form.github_username">
       <span v-if="form.errors.github_username" class="text-danger">{{ form.errors.github_username }}</span>
     </div>
 
     <div class="mb-3">
-      <label for="course_id" class="form-label">Course</label>
+      <label for="course_id" class="form-label">
+        {{ $t('students.field.course') }}
+      </label>
       <select id="course_id" class="form-select" v-model="form.course_id">
-        <option value="" disabled>Select a course</option>
+        <option value="" disabled>
+          {{ $t('students.field.course_select') }}
+        </option>
         <option v-for="course in courses" :key="course.id" :value="course.id">{{ course.name }}</option>
       </select>
       <span v-if="form.errors.course_id" class="text-danger">{{ form.errors.course_id }}</span>
     </div>
 
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary">
+      {{ $t('students.submit_edit') }}
+    </button>
   </form>
 </template>
