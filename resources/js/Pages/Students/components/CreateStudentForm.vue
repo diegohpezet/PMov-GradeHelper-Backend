33bbffb57b8dc6c99ac1f0624426f42999f29337
Tehuel <torresbaldi.tehuel@gmail.com>
2025-02-16 21:33:11 -0300

    create student

--- resources/js/Pages/Students/components/CreateStudentForm.vue
+++ resources/js/Pages/Students/components/CreateStudentForm.vue
@@ -31,32 +31,59 @@ function submit() {
   <form @submit.prevent="submit">
     <div class="row g-2">
       <div class="col-12 col-sm-6 col-xxl-3 my-3">
-        <label for="last_name" class="visually-hidden">Last Name</label>
-        <input type="text" id="last_name" class="form-control" v-model="form.last_name" placeholder="Last Name"/>
+        <label for="last_name" class="visually-hidden">
+          {{ $t('students.field.last_name') }}
+        </label>
+        <input
+          type="text"
+          id="last_name"
+          class="form-control"
+          v-model="form.last_name"
+          :placeholder="$t('students.field.last_name')"
+        />
       </div>
 
       <div class="col-12 col-sm-6 col-xxl-3 my-3">
-        <label for="first_name" class="visually-hidden">First Name</label>
-        <input type="text" id="first_name" class="form-control" v-model="form.first_name" placeholder="First Name" />
+        <label for="first_name" class="visually-hidden">
+          {{ $t('students.field.first_name') }}
+        </label>
+        <input
+          type="text"
+          id="first_name"
+          class="form-control"
+          v-model="form.first_name"
+          :placeholder="$t('students.field.first_name')"
+        />
       </div>
 
       <div class="col-12 col-sm-6 col-xxl-3 my-3">
-        <label for="github_username" class="visually-hidden">Github</label>
-        <input type="text" id="github_username" class="form-control" v-model="form.github_username" placeholder="Github" />
+        <label for="github_username" class="visually-hidden">
+          {{ $t('students.field.github_username') }}
+        </label>
+        <input
+          type="text"
+          id="github_username"
+          class="form-control"
+          v-model="form.github_username"
+          :placeholder="$t('students.field.github_username')"
+        />
       </div>
 
       <div class="col-12 col-sm-6 col-xxl-2 my-3">
-        <label for="course_id" class="visually-hidden">Course</label>
+        <label for="course_id" class="visually-hidden">
+          {{ $t('students.field.course') }}
+        </label>
         <select id="course_id" class="form-select" v-model="form.course_id">
-          <option value="" disabled>Select a course</option>
+          <option value="" disabled>
+            {{ $t('students.field.course_select') }}
+          </option>
           <option v-for="course in courses" :key="course.id" :value="course.id">{{ course.name }}</option>
         </select>
       </div>
 
-      <div class="col-12 col-xxl-1 my-lg-3 d-flex">
-        <button type="submit" class="btn btn-primary text-white w-variant">
-          <i class="ri-add-line"></i>
-          <span class="d-inline d-lg-none">Submit</span>
+      <div class="col-12">
+        <button type="submit" class="btn btn-primary">
+          {{ $t('students.submit') }}
         </button>
       </div>
     </div>
@@ -66,11 +93,3 @@ function submit() {
     </ul>
   </form>
 </template>
-
-<style scoped>
-@media (max-width: 1398px) {
-  .w-variant {
-    width: 100%;
-  }
-}
-</style>
