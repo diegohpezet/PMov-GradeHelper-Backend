e978e55e81abe366d9587d482a51dbf19dafa11e
Tehuel <torresbaldi.tehuel@gmail.com>
2025-02-18 19:16:04 -0300

    lint lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,17 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginVue from "eslint-plugin-vue";
-import configPrettier from "@vue/eslint-config-prettier";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import configPrettier from '@vue/eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,vue}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...pluginVue.configs["flat/recommended"],
-  configPrettier,
+    {
+        files: ['resources/js/*.{js,mjs,cjs,vue}']
+    },
+    {
+        languageOptions: { globals: globals.browser }
+    },
+    pluginJs.configs.recommended,
+    ...pluginVue.configs['flat/recommended'],
+    configPrettier,
 ];
