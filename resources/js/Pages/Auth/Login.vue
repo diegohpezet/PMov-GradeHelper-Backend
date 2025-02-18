eaa754d30b96c2616333d947b9640b0be1b31615
Tehuel <torresbaldi.tehuel@gmail.com>
2025-02-17 23:53:06 -0300

    fix login dark

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -25,66 +25,76 @@ function handleSubmit() {
 </script>
 
 <template>
-  <div class="container bg-light p-3 rounded border">
+  <div class="container">
     <div v-if="status" class="mb-4 text-success">
       {{ status }}
     </div>
 
-    <form @submit.prevent="handleSubmit">
-      <div class="mb-3">
-        <label for="email" value="Email" class="form-label">
-          {{ $t('login.fields.email') }}
-        </label>
-        <input
-          id="email"
-          v-model="form.email"
-          type="email"
-          class="form-control"
-          :disabled="form.processing"
-          autofocus
-        />
-        <span v-if="form.errors.email" class="text-danger">{{
-          form.errors.email
-        }}</span>
-      </div>
+    <div class="card mx-auto shadow-sm">
+      <div class="card-body">
+        <form @submit.prevent="handleSubmit">
+          <div class="mb-3">
+            <label for="email" value="Email" class="form-label">
+              {{ $t('login.fields.email') }}
+            </label>
+            <input
+              id="email"
+              v-model="form.email"
+              type="email"
+              class="form-control"
+              :disabled="form.processing"
+              autofocus
+            />
+            <span v-if="form.errors.email" class="text-danger">{{
+              form.errors.email
+            }}</span>
+          </div>
 
-      <div class="mb-3">
-        <label for="password" class="form-label">
-          {{ $t('login.fields.password') }}
-        </label>
-        <input
-          id="password"
-          v-model="form.password"
-          type="password"
-          class="form-control"
-          :disabled="form.processing"
-        />
-        <span v-if="form.errors.password" class="text-danger">{{
-          form.errors.password
-        }}</span>
-      </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">
+              {{ $t('login.fields.password') }}
+            </label>
+            <input
+              id="password"
+              v-model="form.password"
+              type="password"
+              class="form-control"
+              :disabled="form.processing"
+            />
+            <span v-if="form.errors.password" class="text-danger">{{
+              form.errors.password
+            }}</span>
+          </div>
 
-      <div class="mb-3">
-        <input
-          :id="`checkbox-remember`"
-          type="checkbox"
-          v-model:="form.remember"
-          class="form-check-input"
-        />
-        <label class="form-check-label ms-2" for="checkbox-remember">
-          {{ $t('login.fields.remember') }}
-        </label>
-      </div>
+          <div class="mb-3">
+            <input
+              :id="`checkbox-remember`"
+              type="checkbox"
+              v-model:="form.remember"
+              class="form-check-input"
+            />
+            <label class="form-check-label ms-2" for="checkbox-remember">
+              {{ $t('login.fields.remember') }}
+            </label>
+          </div>
 
-      <div class="mb-3 d-flex align-items-center justify-content-end">
-        <button
-          type="submit"
-          class="btn btn-primary ms-4"
-          :disabled="form.processing"
-        >
-          {{ $t('login') }}
-        </button>
+          <div class="mb-3 d-flex align-items-center justify-content-end">
+            <button
+              type="submit"
+              class="btn btn-primary ms-4"
+              :disabled="form.processing"
+            >
+              {{ $t('login') }}
+            </button>
+          </div>
+        </form>
       </div>
-    </form>
+    </div>
   </div>
 </template>
+
+<style scoped type="text/css">
+div.card {
+  max-width: 400px;
+}
+</style>
