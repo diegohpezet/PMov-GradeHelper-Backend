aacce41d11b95e227cd782d97682ac321c041a28
Tehuel <torresbaldi.tehuel@gmail.com>
2025-03-05 21:49:02 +0000

    floating labels

--- resources/js/Pages/GradeFullscreen/StudentExerciseGradeForm.vue
+++ resources/js/Pages/GradeFullscreen/StudentExerciseGradeForm.vue
@@ -47,38 +47,55 @@ const TYPE_TE_OPTIONS = {
 </script>
 
 <template>
-  <div class="row g-1">
+  <div class="row g-2">
+    <!-- Grade Type -->
     <div class="col-12 col-md">
-      <!-- Grade Type -->
-      <label :for="`grade-type-${formKey}`" class="form-label">
-        {{ $t('grades.field.gradeable_type') }}
-      </label>
-      <select
-        :id="`grade-type-${formKey}`"
-        v-model="model['grade_type']"
-        class="form-select"
-      >
-        <option disabled selected value="">
-          {{ $t('grades.select_grade_type') }}
-        </option>
-        <option
-          v-for="gradeType in GRADE_TYPES"
-          :key="gradeType"
-          :value="gradeType"
+      <div class="form-floating">
+        <select
+          :id="`grade-type-${formKey}`"
+          v-model="model['grade_type']"
+          class="form-select"
         >
-          {{ $t(`grades.grade_types.${gradeType}`) }}
-        </option>
-      </select>
+          <option
+            v-for="gradeType in GRADE_TYPES"
+            :key="gradeType"
+            :value="gradeType"
+          >
+            {{ $t(`grades.grade_types.${gradeType}`) }}
+          </option>
+        </select>
+        <label :for="`grade-type-${formKey}`">
+          {{ $t('grades.field.gradeable_type') }}
+        </label>
+      </div>
     </div>
+
+    <!-- Grade Result -->
     <div class="col-12 col-md">
-      <!-- Grade Value -->
-      <label class="form-label">
-        {{ $t('grades.field.gradeable_value') }}
-      </label>
+      <!-- Numeric Grade -->
+      <div
+        v-if="model['grade_type'] === GRADE_TYPES.NUMERIC"
+        class="form-floating"
+      >
+        <input
+          :id="`grade-number-${formKey}`"
+          v-model="model['grade_value']"
+          class="form-control"
+          type="number"
+          :placeholder="$t('grades.field.gradeable_value')"
+          min="0"
+          max="10"
+          step="1"
+        />
+        <label :for="`grade-number-${formKey}`">
+          {{ $t('grades.field.gradeable_value') }}
+        </label>
+      </div>
 
       <!-- Pass/Fail -->
+      <!-- Pass/Fail -->
       <div v-if="model['grade_type'] === GRADE_TYPES.PASS_FAIL">
-        <div class="btn-group w-100" role="group">
+        <div class="btn-group btn-group-custom-h w-100" role="group">
           <input
             :id="`grade-passfail-pass-${formKey}`"
             v-model="model['grade_value']"
@@ -115,7 +132,7 @@ const TYPE_TE_OPTIONS = {
 
       <!-- TE Grade -->
       <div v-if="model['grade_type'] === GRADE_TYPES.TEG">
-        <div class="btn-group w-100" role="group">
+        <div class="btn-group btn-group-custom-h w-100" role="group">
           <template v-for="teOption in TYPE_TE_OPTIONS" :key="teOption.name">
             <input
               :id="`grade-te-${teOption.name}-${formKey}`"
@@ -136,33 +153,33 @@ const TYPE_TE_OPTIONS = {
           </template>
         </div>
       </div>
-
-      <!-- Numeric Grade -->
-      <div v-if="model['grade_type'] === GRADE_TYPES.NUMERIC">
-        <input
-          :id="`grade-number-${formKey}`"
-          v-model="model['grade_value']"
-          class="form-control"
-          type="number"
-          min="0"
-          max="10"
-          step="1"
-        />
-      </div>
     </div>
+
+    <!-- Comment -->
     <div class="col-12">
-      <!-- Comment -->
-      <label :for="`comment-${formKey}`" class="form-label">
-        {{ $t('grades.field.comment') }}
-      </label>
-      <input
-        :id="`comment-${formKey}`"
-        v-model="model['comment']"
-        type="text"
-        class="form-control"
-      />
+      <div class="form-floating">
+        <textarea
+          :id="`comment-${formKey}`"
+          v-model="model['comment']"
+          class="form-control"
+          rows="3"
+          :placeholder="$t('grades.field.comment')"
+        ></textarea>
+        <label :for="`comment-${formKey}`">
+          {{ $t('grades.field.comment') }}
+        </label>
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.btn-group-custom-h {
+  height: calc(3.5rem + calc(var(--bs-border-width) * 2));
+  min-height: calc(3.5rem + calc(var(--bs-border-width) * 2));
+
+  label {
+    line-height: 2.8rem;
+  }
+}
+</style>
