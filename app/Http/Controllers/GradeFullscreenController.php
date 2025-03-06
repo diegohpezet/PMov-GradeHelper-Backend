b9a0034d92531663236dd46ed7826582b250fa68
Tehuel <torresbaldi.tehuel@gmail.com>
2025-03-06 00:52:04 +0000

    make controller work

--- app/Http/Controllers/GradeFullscreenController.php
+++ app/Http/Controllers/GradeFullscreenController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Assessment;
 use App\Models\Course;
+use App\Models\Grade;
+use App\Enums\GradeableType;
 use App\Http\Requests\BatchGradeRequest;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -22,7 +25,38 @@ class GradeFullscreenController extends Controller
 
     public function batchGrade(Course $course, BatchGradeRequest $request)
     {
-        dd($request);
+        // TODO: validate we're creating permitted grades
+        // - user has permission
+        // - user is in course
+        // - every student and exercise are in course
+
+        $gradesRequest = collect($request->grades);
+
+        $assessmentIdByExerciseId = $course
+            ->assessments()
+            ->get(['id', 'exercise_id'])
+            ->mapWithKeys(function (Assessment $assessment) {
+                return [$assessment['exercise_id'] => $assessment['id']];
+            });
+
+        $gradesRequest->each(function ($gradeRequest) use ($assessmentIdByExerciseId) {
+            $gradeableTypeModel = GradeableType::from($gradeRequest['grade_type'])->model();
+
+            // creates a new gradeable
+            $gradeableInstance = ($gradeableTypeModel)::create([
+                'value' => $gradeRequest['grade_value'],
+                'comment' => $gradeRequest['comment'],
+            ]);
+
+            Grade::create([
+                'assessment_id' => $assessmentIdByExerciseId[$gradeRequest['exercise']],
+                'student_id' => $gradeRequest['student'], 
+                'gradeable_id' => $gradeableInstance->id,
+                'gradeable_type'=> $gradeableTypeModel,
+            ]);
+
+            // TODO: mass create all grades at once
+        });
 
         return redirect()
             ->back()
